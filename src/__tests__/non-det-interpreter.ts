ee6c97ee80402eeb3e9409a2015d82b71b0fe22e
anubh-v <e0176401@u.nus.edu>
2020-03-23 16:16:41 +0800

    Add tests for list functionality

--- src/__tests__/non-det-interpreter.ts
+++ src/__tests__/non-det-interpreter.ts
@@ -11,11 +11,27 @@ test('Empty code returns undefined', async () => {
 test('Deterministic calculation', async () => {
   await testDeterministicCode('1 + 4 - 10 * 5;', -45)
 })
+
+test('Test builtin list functions', async () => {
+  await testDeterministicCode('pair(false, 10);', [false, 10])
+  await testDeterministicCode('list();', null)
+  await testDeterministicCode('list(1);', [1, null])
+  await testDeterministicCode('head(list(1));', 1)
+  await testDeterministicCode('tail(list(1));', null)
+  await testDeterministicCode(
+    `function increment(x) {
+      x + 1;
+    }
+    map(increment, list(1,2,3));`,
+    [2,[3,[4, null]]]
+  )
+})
 // ---------------------------------- Non deterministic code tests -------------------------------
 
 test('Test simple amb application', async () => {
   await testNonDeterministicCode('amb(1, 4 + 5, 3 - 10);', [1, 9, -7])
 })
+
 
 // ---------------------------------- Helper functions  -------------------------------------------
 
@@ -35,14 +51,14 @@ async function testNonDeterministicCode(code: string, expectedValues: any[]) {
   let result: Result = await runInContext(code, context, nonDetTestOptions)
   const numOfRuns = expectedValues.length
   for (let i = 0; i < numOfRuns; i++) {
-    expect((result as SuspendedNonDet).value).toBe(expectedValues[i])
-    expect(result.status).toBe('suspended-non-det')
+    expect((result as SuspendedNonDet).value).toEqual(expectedValues[i])
+    expect(result.status).toEqual('suspended-non-det')
     result = await resume(result)
   }
 
   // all non deterministic programs have a final result whose value is undefined
-  expect(result.status).toBe('finished')
-  expect((result as Finished).value).toBe(undefined)
+  expect(result.status).toEqual('finished')
+  expect((result as Finished).value).toEqual(undefined)
 }
 
 function makeNonDetContext() {
