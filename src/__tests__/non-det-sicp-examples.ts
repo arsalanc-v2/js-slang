dd5d11e3e1911af362859cb5dc59b71f7b44321a
anubh-v <e0176401@u.nus.edu>
2020-03-29 03:11:25 +0800

    Add an_element_of

--- src/__tests__/non-det-sicp-examples.ts
+++ src/__tests__/non-det-sicp-examples.ts
@@ -1,9 +1,21 @@
 /* This file uses programs from SICP JS 4.3 as tests for the non deterministic interpreter */
 import { testNonDeterministicCode } from './non-det-interpreter'
 
+test('An element of', async () => {
+  await testNonDeterministicCode(
+      `function an_element_of(items) {
+        require(!is_null(items));
+        return amb(head(items), an_element_of(tail(items)));
+       }
+
+       an_element_of(list(1, 2, list(3, 4)));`,
+       [1, 2, [3, [4, null]]])
+})
+
+
 test('Multiple dwelling problem', async () => {
-    await testNonDeterministicCode(
-        `function multiple_dwelling() {
+  await testNonDeterministicCode(
+    `function multiple_dwelling() {
             const baker = amb(1, 2, 3, 4, 5);
             const cooper = amb(1, 2, 3, 4, 5);
             const fletcher = amb(1, 2, 3, 4, 5);
@@ -24,14 +36,20 @@ test('Multiple dwelling problem', async () => {
                         list("smith", smith));
         }
         multiple_dwelling();`,
-        [[
-            ["baker", [3, null]],
-            [["cooper", [2, null]],
-            [["fletcher", [4, null]],
-            [["miller", [5, null]],
-            [["smith", [1, null]],
-            null]]]]
-        ]])
-
-
+    [
+      [
+        ['baker', [3, null]],
+        [
+          ['cooper', [2, null]],
+          [
+            ['fletcher', [4, null]],
+            [
+              ['miller', [5, null]],
+              [['smith', [1, null]], null]
+            ]
+          ]
+        ]
+      ]
+    ]
+  )
 })
