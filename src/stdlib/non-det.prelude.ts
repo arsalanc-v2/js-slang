b12478d27d3a44595086668cd5b29be2a5680d25
Arsalan Cheema <arsalanc.v2@gmail.com>
2020-03-29 09:59:40 +0800

    Add tests and textbook examples (#11)
    - Add various tests, including textbook examples
    - Standardize their naming
    - Reorder tests.
    - Add an_integer_between and an_element_of to the non deterministic prelude
    
    Co-authored-by: anubh-v <e0176401@u.nus.edu>

--- src/stdlib/non-det.prelude.ts
+++ src/stdlib/non-det.prelude.ts
@@ -12,4 +12,13 @@ export const nonDetPrelude = `
                 ? distinct(tail(items))
                 : false;
     }
+
+    function an_element_of(items) {
+        require(!is_null(items));
+        return amb(head(items), an_element_of(tail(items)));
+    }
+
+    function an_integer_between(low, high) {
+        return low > high ? amb() : amb(low, an_integer_between(low + 1, high));
+    }
 `
