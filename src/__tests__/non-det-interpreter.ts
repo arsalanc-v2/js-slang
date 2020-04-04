4f2299d980f4978f5a44103c10c02945c503cfe5
Arsalan Cheema <arsalanc.v2@gmail.com>
2020-04-04 23:54:01 +0800

    add implication and bi-implication

--- src/__tests__/non-det-interpreter.ts
+++ src/__tests__/non-det-interpreter.ts
@@ -155,7 +155,6 @@ test('Function applications', async () => {
      }`,
     undefined
   )
-
   await testDeterministicCode(`const a = 2; a();`, 'Line 1: Calling non-function value 2.', true)
 
   await testDeterministicCode(
@@ -379,7 +378,25 @@ test('Block statements', async () => {
     true
   )
 })
+test('Material Conditional', async () => {
+  await testDeterministicCode(`implication(true, true);`, true)
 
+  await testDeterministicCode(`implication(true, false);`, false)
+
+  await testDeterministicCode(`implication(false, true);`, true)
+
+  await testDeterministicCode(`implication(false, false);`, true)
+})
+
+test('Material Biconditional', async () => {
+  await testDeterministicCode(`bi_implication(true, true);`, true)
+
+  await testDeterministicCode(`bi_implication(true, false);`, false)
+
+  await testDeterministicCode(`bi_implication(false, true);`, false)
+
+  await testDeterministicCode(`bi_implication(false, false);`, true)
+})
 // ---------------------------------- Helper functions  -------------------------------------------
 
 const nonDetTestOptions = {
